a5c947fde52edf777454539b84f1aa9bcbfc1656
YazRas <96800707+YazRas@users.noreply.github.com>
2023-10-03 20:48:56 +0000

    Added comments

--- motor.py
+++ motor.py
@@ -1,79 +1,90 @@
-import RPi.GPIO as GPIO          
+# Import the RPi.GPIO library for Raspberry Pi GPIO control
+import RPi.GPIO as GPIO
+# Import the sleep function from the time module
 from time import sleep
 
-in1 = 24
-in2 = 23
+# Define the GPIO pins for the motor inputs and enable pin
+input1 = 24
+input2 = 23
 en = 25
-temp1=1
+temp1 = 1  # A variable to store the motor direction
 
+# Set the GPIO mode to use the BCM numbering scheme
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-p=GPIO.PWM(en,1000)
+# Set up the GPIO pins as outputs
+GPIO.setup(input1, GPIO.OUT)
+GPIO.setup(input2, GPIO.OUT)
+GPIO.setup(en, GPIO.OUT)
+# Set the initial states of the motor inputs to LOW
+GPIO.output(input1, GPIO.LOW)
+GPIO.output(input2, GPIO.LOW)
+
+# Create a PWM (Pulse Width Modulation) object on the enable pin
+p = GPIO.PWM(en, 1000)
+# Start the PWM with a duty cycle of 25%
 p.start(25)
+
+# Print initial messages to the console
 print("\n")
-print("The default speed & direction of motor is LOW & Forward.....")
+print("The default speed & direction of the motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-print("\n")    
+print("\n")
 
-while(1):
-
-    x=input()
+# Enter an infinite loop to control the motor based on user input
+while True:
+    x = input()  # Read user input
     
-    if x=='r':
+    # Check user input and control the motor accordingly
+    if x == 'r':
         print("run")
-        if(temp1==1):
-         GPIO.output(in1,GPIO.HIGH)
-         GPIO.output(in2,GPIO.LOW)
-         print("forward")
-         x='z'
+        if temp1 == 1:
+            GPIO.output(input1, GPIO.HIGH)
+            GPIO.output(input2, GPIO.LOW)
+            print("forward")
+            x = 'z'
         else:
-         GPIO.output(in1,GPIO.LOW)
-         GPIO.output(in2,GPIO.HIGH)
-         print("backward")
-         x='z'
-
-
-    elif x=='s':
+            GPIO.output(input1, GPIO.LOW)
+            GPIO.output(input2, GPIO.HIGH)
+            print("backward")
+            x = 'z'
+    
+    elif x == 's':
         print("stop")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-        x='z'
-
-    elif x=='f':
+        GPIO.output(input1, GPIO.LOW)
+        GPIO.output(input2, GPIO.LOW)
+        x = 'z'
+    
+    elif x == 'f':
         print("forward")
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in2,GPIO.LOW)
-        temp1=1
-        x='z'
-
-    elif x=='b':
+        GPIO.output(input1, GPIO.HIGH)
+        GPIO.output(input2, GPIO.LOW)
+        temp1 = 1
+        x = 'z'
+    
+    elif x == 'b':
         print("backward")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.HIGH)
-        temp1=0
-        x='z'
-
-    elif x=='l':
+        GPIO.output(input1, GPIO.LOW)
+        GPIO.output(input2, GPIO.HIGH)
+        temp1 = 0
+        x = 'z'
+    
+    elif x == 'l':
         print("low")
         p.ChangeDutyCycle(25)
-        x='z'
-
-    elif x=='m':
+        x = 'z'
+    
+    elif x == 'm':
         print("medium")
         p.ChangeDutyCycle(50)
-        x='z'
-
-    elif x=='h':
+        x = 'z'
+    
+    elif x == 'h':
         print("high")
         p.ChangeDutyCycle(75)
-        x='z'
-     
+        x = 'z'
     
-    elif x=='e':
+    elif x == 'e':
+        # Cleanup GPIO and exit the program when 'e' is entered
         GPIO.cleanup()
         break
     
